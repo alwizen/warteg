7c9f4cf968669c6b2c4c96da08afa9043147ced5
alwizen <solu8i.project@gmail.com>
2025-04-19 10:38:48 +0700

    add cors

---
+++ be/config/cors.php
@@ -0,0 +1,8 @@
+<?php
+return[
+    'paths' => ['api/*'],
+'allowed_origins' => ['*'], // Bisa ganti dengan URL spesifik untuk lebih aman
+'allowed_methods' => ['*'],
+'allowed_headers' => ['*'],
+
+];
